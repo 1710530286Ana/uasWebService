3ee3cd78367cc7e23467a48fc1a3f6de34146390
isrocacing <isrocacing01@gmail.com>
2020-08-04 13:50:44 +0800

    Uas laravel

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,20 @@
+@extends('app')
+
+@section('title')
+    Index
+@endsection
+
+@section('content')
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <h4><i class="fa fa-info-circle"></i> Sistem Aplikasi Pencarian Buku Olahraga</h4>
+            <hr>
+            <p align="justify">
+        	    Selamat Membaca
+            </p>
+            <p align="right">
+              	<a href="/buku"><button type="button" class="btn btn-primary btn-lg">START&nbsp;&nbsp;<i class="fa fa-arrow-circle-right"></i></button></a>
+            </p>
+        </div>
+    </div>
+@endsection
